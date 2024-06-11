a4844bca11c6db5e2e9550f9706de24e01376b59
halowalletdev <155522934+halowalletdev@users.noreply.github.com>
2024-06-11 10:02:00 +0800

    Delete interfaces directory

--- interfaces/IERC20WithDecimals.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IERC20WithDecimals is IERC20 {
-    // the return type for most erc20s is uint8
-    // in order to be compatible with special cases, there use uint256
-    function decimals() external view returns (uint256);
-}
